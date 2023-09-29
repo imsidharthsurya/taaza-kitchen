19748268320d00fc561ff5a439f78e4b4831ddc2
Sidharth Surya <imsidharthsurya@gmail.com>
2023-09-29 20:33:38 +0530

    include react & bundler

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const heading=React.createElement("h1",{className:"temp"},"Taaza Kitchen");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
