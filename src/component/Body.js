3bb56c4e1df57d598ba8c9ca5031b46e61b86967
Sidharth Surya <imsidharthsurya@gmail.com>
2023-09-30 23:19:06 +0530

    add search functionality using usestate to update & re-render the state after each update

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,14 +1,37 @@
 import {restrauntData} from "./config"
 import RestrauntCard from "./RestrauntCard"
+import { useState } from "react"
+
+function filterData(searchText,restrauntList){
+    return restrauntData.filter((restraunt)=>{
+        return restraunt.info.name.includes(searchText);
+    })
+}
 const Body = () => {
+    console.log("body component loaded")
+    const[searchText,setSearchText]=useState("")
+    const[restrauntList,setRestrauntList]=useState(restrauntData)
     return (
-        <div className="restraunt-data">
-            {
-                restrauntData.map((restraunt)=>{
-                    return <RestrauntCard {...restraunt.info} key={restraunt.info.id}/>
-                })
-            }
-        </div>
+        <>
+            <div className="search-div">
+                <input type="text" placeholder="search" value={searchText} onChange={(e)=>{
+                    console.log(e.target.value)
+                    setSearchText(e.target.value)
+                }}/>
+                <button className="search-btn" onClick={()=>{
+                    const finalData=filterData(searchText,restrauntList)
+                    console.log("the final data after filter is: ",finalData)
+                    setRestrauntList(finalData)
+                }}>Search</button>
+            </div>
+            <div className="restraunt-data">
+                {
+                    restrauntList.map((restraunt)=>{
+                        return <RestrauntCard {...restraunt.info} key={restraunt.info.id}/>
+                    })
+                }
+            </div>
+        </>
     )
 }
 
