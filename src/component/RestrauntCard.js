a08475c09c4466dd2236b1906a991f3084efcc65
Sidharth Surya <imsidharthsurya@gmail.com>
2023-10-03 23:20:46 +0530

    add locality

--- src/component/RestrauntCard.js
+++ src/component/RestrauntCard.js
@@ -1,12 +1,14 @@
 import {IMG_URL} from "./config"
-const RestrauntCard = ({name,cuisines,avgRating,cloudinaryImageId}) => {
+const RestrauntCard = ({name,cuisines,avgRating,cloudinaryImageId,areaName}) => {
     // const {name,cuisines,avgRating,cloudinaryImageId}=restaurant.info
     return (
         <div className="restraunt-card">
             <img className="rest-pic" src={IMG_URL+cloudinaryImageId} alt="restaunt pic" />
             <h3>{name}</h3>
-            <h4>{cuisines.join(", ")}</h4>
-            <h5>{avgRating} ☆</h5>
+            <h4>⭐{avgRating}</h4>
+            <p>{cuisines.join(", ")}</p>
+            
+            <p>{areaName}</p>
         </div>
     )
 }
