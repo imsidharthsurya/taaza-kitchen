e33f404a2e9d2c2abe10ccddc2274c28744a8a95
Sidharth Surya <imsidharthsurya@gmail.com>
2023-10-04 21:34:24 +0530

    add shimmer UI

--- src/component/ShimmerUI.js
+++ src/component/ShimmerUI.js
@@ -1,7 +1,16 @@
 const ShimmerUI=()=>{
     return (
-        <div>
-            <h1>Shimmer UI Loading...</h1>
+        <div className="restraunt-data">
+            {
+                new Array(30).fill(1).map((ele,i)=>{
+                    return (<div className="restraunt-card">
+                    <div className="shimmer-card"></div>
+                    <div className="second-shimmer"></div>
+                    <div className="third-shimmer"></div>
+                </div>)
+                })
+            
+            }
         </div>
     )
 }
