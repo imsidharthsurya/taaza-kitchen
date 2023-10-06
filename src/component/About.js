088017a092ae5f18473e7de37070f31adcd92dcd
Sidharth Surya <imsidharthsurya@gmail.com>
2023-10-06 22:01:04 +0530

    add basic routing using react router createBrowserRouter and RouterProvider

---
+++ src/component/About.js
@@ -0,0 +1,10 @@
+const About=()=>{
+    return(
+        <div>
+            <h1>About Me</h1>
+            <h3>I am sidharth surya</h3>
+        </div>
+    )
+}
+
+export default About
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client"
 import Body from "./component/Body"
 import Footer from "./component/Footer"
 import Header from "./component/Header"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Contact from "./component/Contact"
+import About from "./component/About"
 
 const AppLayout = () => {
     return (
@@ -14,5 +17,19 @@ const AppLayout = () => {
     )
 }
 
+const router=createBrowserRouter([
+    {
+        path:"/",
+        element:<AppLayout/>
+    },{
+        path:"/about",
+        element:<About/>
+    },{
+        path:"/contact",
+        element:<Contact/>
+    }
+])
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<AppLayout />)
+root.render(<RouterProvider router={router}/>)
